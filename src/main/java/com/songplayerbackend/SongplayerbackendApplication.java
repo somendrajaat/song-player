821803e6b48ce5e0d5f37f58dec8adc22f4e3af0
Somendra <somendrajaata@gmail.com>
2024-11-07 08:20:49 +0530

    setting up environment

---
+++ src/main/java/com/songplayerbackend/SongplayerbackendApplication.java
@@ -0,0 +1,13 @@
+package com.songplayerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongplayerbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SongplayerbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/songplayerbackend/SongplayerbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songplayerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongplayerbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
